0b3aa48b42b5c8c5256f75f4fa322c9ec54a7847
Arthur Penteado <ascpenteado@gmail.com>
2021-03-04 20:22:11 -0300

    add accounts migration

--- src/database/migrations/1614736407156-CreateAccountsTable.ts
+++ src/database/migrations/1614736407156-CreateAccountsTable.ts
@@ -1,11 +1,35 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateAccountsTable1614736407156 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "accounts",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "balance",
+            type: "decimal",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("accounts");
+  }
 }

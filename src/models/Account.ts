6f0273ef6c4e0740b795cf65ac112dc2442fe1a6
Arthur Penteado <ascpenteado@gmail.com>
2021-03-06 12:26:25 -0300

    add reference to Transactions in Account model

--- src/models/Account.ts
+++ src/models/Account.ts
@@ -1,17 +1,26 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
+import { Transaction } from "../models/Transaction";
 
 @Entity("accounts")
 class Account {
-  @PrimaryColumn() readonly id: string;
+  @PrimaryColumn()
+  readonly id: string;
 
-  @Column() name: string;
+  @Column()
+  name: string;
 
-  @Column() description: string;
+  @Column()
+  description: string;
 
-  @Column() balance: number;
+  @Column()
+  balance: number;
 
-  @CreateDateColumn() created_at: Date;
+  @CreateDateColumn()
+  created_at: Date;
+
+  @OneToMany(() => Transaction, (transaction) => transaction.account)
+  transactions: Transaction[];
 
   constructor() {
     if (!this.id) {
